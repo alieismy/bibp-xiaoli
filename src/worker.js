b2b4a8af3a1042ae51efdf329350adffbcc5f18b
Lee Johnson <alieismy@gmail.com>
2025-02-06 16:34:56 +0800

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -6,7 +6,7 @@ import { SignJWT, jwtVerify } from 'jose';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '99ab5f04-6f6a-4dd3-99d1-040b43654601';
 let trojanPassword = `bpb-trojan`;
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
